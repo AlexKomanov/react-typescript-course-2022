319f938878fdd99fd27dd6eed7a4c360d33a4076
AlexKomanov <akomanov88@gmail.com>
2022-08-05 09:05:16 +0300

    Adding functions to the CreateProducts.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Product from "./components/Product";
 import {useProducts} from "./hooks/products";
 import Loader from "./components/Loader";
@@ -9,15 +9,16 @@ import CreateProduct from "./components/CreateProduct";
 const App = () => {
 
     const {loading, error, items} = useProducts();
+    const [modal, setModal] = useState(true);
 
     return (
         <div className="container rounded mx-auto max-w-2xl pt-5">
             {loading && <Loader />}
             {error && <ErrorMessage error={error} />}
             {items.map(item => <Product product={item} key={item.id}/>)}
-            <Modal title="Add new product">
-                <CreateProduct />
-            </Modal>
+            {modal && <Modal title="Add new product">
+                <CreateProduct onCreate={() => setModal(false)}/>
+            </Modal>}
         </div>
     );
 }
--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,14 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {IProduct} from "../models";
+import product from "./Product";
+import axios from "axios";
+import ErrorMessage from "./ErrorMessage";
 
-const CreateProduct = () => {
+const mockingData: IProduct = {
+    title: 'test product',
+    price: 13.5,
+    description: 'lorem ipsum set',
+    image: 'https://i.pravatar.cc',
+    category: 'electronic',
+    rating: {
+        rate: 42,
+        count: 10
+    }
+}
+
+interface CreateProductProps {
+    onCreate: () => void;
+}
+
+const CreateProduct = ({onCreate} : CreateProductProps) => {
+    const [value, setValue] = useState('')
+    const [error, setError] = useState('')
+
+    const submitHandler = async (event: React.FormEvent) => {
+        event.preventDefault();
+
+
+        if(value.trim().length === 0) {
+            setError('Please enter a valid title')
+            return
+        }
+
+        mockingData.title = value
+        const response = await axios.post<IProduct>('https://fakestoreapi.com/products', mockingData)
+        console.log(response);
+
+        onCreate();
+    }
+
+    const changeHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        setValue(event.target.value)
+        setError('')
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <input
                 type="text"
                 className="border py-2 px-4 mb-2 w-full outline-0"
                 placeholder="Enter product title..."
+                value={value}
+                onChange={changeHandler}
             />
-            <button type={"submit"} className={"py-2 px-4 border rounded-[15px] bg-yellow-400 hover:bg-blue-700 hover:text-white"}>
+
+            {error && <ErrorMessage error={error} />}
+
+            <button type={"submit"}
+                    className={"py-2 px-4 border rounded-[15px] bg-yellow-400 hover:bg-blue-700 hover:text-white"}>
                 Create
             </button>
 
